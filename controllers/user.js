d30012ae096329c5e6c1f1b8792476152715c5bb
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-08-02 18:05:43 +0300

    feat: заменить версию mogo@4.2 написать контроллер user

---
+++ controllers/user.js
@@ -0,0 +1,31 @@
+const User = require("../models/user");
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((error) => {
+      res.status(500).send({ message: `Ошибка сервера ${error}` });
+    });
+};
+module.exports.getUserById = (req, res) => {
+  const userId = req.params.id;
+  User.findById(userId)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((error) => {
+      res.status(500).send({ message: `Ошибка сервера ${error}` });
+    });
+};
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((error) => {
+      res.status(500).send({ message: `Ошибка сервера ${error}` });
+    });
+};
