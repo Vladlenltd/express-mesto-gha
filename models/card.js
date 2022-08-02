a8141574a4b82e99d6a0eb3b49a0e76853dffb4d
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-08-02 13:50:59 +0300

    feat:исправить бэйджи, сделать схемы

---
+++ models/card.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    minlength: 2,
+    maxlenght: 30,
+  },
+  link: {
+    type: String,
+    require: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      default: [],
+    },
+  ],
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("card", cardSchema);
---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    minlength: 2,
+    maxlenght: 30,
+  },
+  about: {
+    type: String,
+    require: true,
+    minlength: 2,
+    maxlenght: 30,
+  },
+  avatar: {
+    type: String,
+    require: true,
+  },
+});
+module.exports = mongoose.model("user", userSchema);
