b47614ac2605208a58b74367233a1032ee3d39dd
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-08-07 22:15:07 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -4,22 +4,28 @@ const errorStatus = require('../utils/errorStatus');
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((data) => {
-      res.status(errorStatus.SUCCESSFUL_REQUEST).send(data);
-    })
+    .then((data) => res.status(200).send(data))
     .catch((error) => {
       if (error.name === 'ValidationError') {
-        res.status(errorStatus.BAD_REQUEST).send({ message: 'Данные не прошли валидацию на сервере' });
-        // return;
-      } else {
-        res.status(errorStatus.SERVER_ERROR).send({ message: `Ошибка сервера ${error}` });
+        return res.status(400).send({ message: 'Вы ввели неверные данные пользователя' });
       }
+      return res.status(500).send({ message: 'На сервере произошла ошибка' });
     });
+  // .then((data) => {
+  //   res.status(errorStatus.SUCCESSFUL_REQUEST).send(data);
+  // })
+  // .catch((error) => {
+  //   if (error.name === 'ValidationError') {
+  // res.status(errorStatus.BAD_REQUEST).send({ message: 'Данные не прошли валидацию на сервере' });
+  //     // return;
+  //   } else {
+  //     res.status(errorStatus.SERVER_ERROR).send({ message: `Ошибка сервера ${error}` });
+  //   }
+  // });
 };
 module.exports.getUserById = (req, res) => {
   const userId = req.params.id;
   User.findById(userId)
-    .orFail(new Error(`Пользователь с указанным id:${userId} не найден`))
     .then((data) => {
       if (!data) {
         res.status(errorStatus.NOT_FOUND).send({ message: `Пользователь с указанным id:${userId} не найден` });
