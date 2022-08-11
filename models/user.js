7e130d3bd9bf376e0fb78be4a2b4630d5aeeeeee
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-08-11 23:32:19 +0300

    feat: подключить validator

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Неверный E-mail!!!',
+    },
   },
   password: {
     type: String,
